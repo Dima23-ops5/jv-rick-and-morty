0532a6fbdbb4d47f73f7da37f8dcb1f9104718bd
Dima <dmmhnk23@gmail.com>
2024-07-09 10:44:11 +0200

    fixed imports

--- src/main/java/mate/academy/rickandmorty/mapper/CharactersMapper.java
+++ src/main/java/mate/academy/rickandmorty/mapper/CharactersMapper.java
@@ -1,12 +1,11 @@
 package mate.academy.rickandmorty.mapper;
 
+import java.util.List;
 import mate.academy.rickandmorty.config.MapperConfig;
 import mate.academy.rickandmorty.dto.external.CharactersResponseResultDto;
 import mate.academy.rickandmorty.dto.internal.CharactersResponseDto;
 import mate.academy.rickandmorty.model.Character;
 import org.mapstruct.Mapper;
-
-import java.util.List;
 
 @Mapper(config = MapperConfig.class)
 public interface CharactersMapper {
