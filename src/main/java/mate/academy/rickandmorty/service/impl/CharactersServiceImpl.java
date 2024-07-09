bf3f1c3fa8d325446c2c1905cf24a65de26892a0
Dima <dmmhnk23@gmail.com>
2024-07-09 11:02:11 +0200

    Added method to CharcterRepository getRandom ()

--- src/main/java/mate/academy/rickandmorty/service/impl/CharactersServiceImpl.java
+++ src/main/java/mate/academy/rickandmorty/service/impl/CharactersServiceImpl.java
@@ -1,28 +1,22 @@
 package mate.academy.rickandmorty.service.impl;
 
 import java.util.List;
-import java.util.Random;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import mate.academy.rickandmorty.dto.internal.CharactersResponseDto;
 import mate.academy.rickandmorty.mapper.CharactersMapper;
 import mate.academy.rickandmorty.repository.CharactersRepository;
-import mate.academy.rickandmorty.service.CharactersClient;
 import mate.academy.rickandmorty.service.CharactersService;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CharactersServiceImpl implements CharactersService {
     private final CharactersMapper charactersMapper;
     private final CharactersRepository charactersRepository;
-    private final CharactersClient charactersClient;
-    private final Random random;
 
     @Override
     public CharactersResponseDto getRandomCharacter() {
-        Long randomId = random.nextLong(
-                charactersClient.getCharacters().info().count());
-        return charactersMapper.toDto(charactersRepository.getById(randomId));
+        return charactersMapper.toDto(charactersRepository.getRandom());
     }
 
     @Override
